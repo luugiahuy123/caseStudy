41e367756663344ed0b20195d52cbcff3e7d5f2e
luugiahuy123 <luugiahuyqn@gmail.com>
2023-11-30 10:12:05 +0700

    huong_dan

--- 2.js
+++ 2.js
@@ -1,19 +1,27 @@
 let canvas = document.getElementById('game');
 let context = canvas.getContext('2d');
+let running = false;
+
 let ball = {
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    dx: 10,
-    dy: 10,
+    dx: 20,
+    dy: 20,
     radius: 50,
 };
+
 function drawBall() {
+    let gradient = context.createLinearGradient(ball.x, ball.y - ball.radius, ball.x, ball.y + ball.radius);
+    gradient.addColorStop(0, '#33ccff');
+    gradient.addColorStop(1, '#ff99cc');
+
     context.beginPath();
     context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    context.fillStyle = "#43d313";
+    context.fillStyle = gradient;
     context.fill();
     context.closePath();
 }
+
 function updateBallPosition() {
     ball.x += ball.dx;
     ball.y += ball.dy;
@@ -27,6 +35,7 @@ function updateBallPosition() {
 }
 
 function run() {
+    if (!running) return;
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     updateBallPosition();
@@ -38,5 +47,36 @@ canvas.addEventListener('click', function (event) {
     let distY = Math.abs(ball.y - event.clientY);
     if (distX < ball.radius && distY < ball.radius) {
         alert('Hit!');
+        running = false;
     }
+});
+
+document.getElementById('start').addEventListener('click', function () {
+    running = true;
+    run();
+    let audio = Math.random();
+    if (audio < 0.25) {
+        audio = 'audio2';
+    } else if (audio < 0.5) {
+        audio = 'audio3';
+    } else if (audio < 0.75) {
+        audio = 'audio4';
+    } else {
+        audio = 'audio5';
+    }
+    document.getElementById(audio).play();
+});
+document.getElementById('increaseSpeed').addEventListener('click', function () {
+    ball.dx *= 1.2;
+    ball.dy *= 1.2;
+});
+document.getElementById('increaseSize').addEventListener('click', function () {
+    ball.radius *= 1.2;
+});
+document.getElementById('downSpeed').addEventListener('click', function () {
+    ball.dx /= 1.2;
+    ball.dy /= 1.2;
+});
+document.getElementById('downSize').addEventListener('click', function () {
+    ball.radius /= 1.2;
 });
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# caseStudy
-Bắt đầu trò chơi 
+# caseStud
+
+Bắt đầu trò chơi:
+
+- Bạn hãy ấn vào nút bắt đầu
+- Có nhiều Chế độ chơi bạn có tăng chỉnh quả bóng nhanh , chậm ,
+  to , nhỏ tuỳ theo bạn muốn chơi chế độ khó hay dể .
+- Để thằng trò chơi bạn chỉ cần đưa con trỏ chuột ấn trúng vào quả bóng .
+  <3 <3 <3 
